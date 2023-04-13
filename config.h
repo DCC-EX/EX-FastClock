223679fe8a3e976f69a89b080cacf92c88c58f27
Colin Murdoch <colin.murdoch@btinternet.com>
2023-04-13 17:19:10 +0100

    Structure changes
    Moved calibration and header to config.h

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 //#define SEND_VIA_I2C true
 
 // If we are sending the data to the CS via Serial then uncomment the following line
-//#define SEND_VIA_SERIAL true
+#define SEND_VIA_SERIAL true
 
 // The following line governs the clock speeds availavle for selection
 // Clockspeed of 1 = real time
@@ -20,4 +20,18 @@ byte clockSpeeds[] = {1, 2, 4, 6, 8, 12, 30};  //  Fast Clock Speeds
 // The following line sets the default (startup) speed.
 
 byte clockSpeed = 4;               //  Initial Fast Clock Speed Setting
+
+
+
+// Set this header to the title you wish to see
+#define HDDR_SIZE  25
+char header[HDDR_SIZE] = {"Nantyderry Junction!"};
+
+// ALL Touch panels and wiring is DIFFERENT
+// copy-paste results from TouchScreen_Calibr_native.ino
+// These settings are for the McuFriend 2.8" shield in Portrait tft.screenRotaion(2)
+
+const byte XP = 7, XM = A1, YP = A2, YM = 6;
+
+const int TS_LEFT = 123, TS_RT = 923, TS_TOP = 895, TS_BOT = 98; // for Portrait orientation 2
 
